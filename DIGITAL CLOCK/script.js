4270bb23bb3d0c875c9da39c9db4b24cfcabdd6c
Vineeth Shetty <shettyvineeth1999@gmail.com>
2023-01-02 16:54:40 +0530

    Update script.js

--- DIGITAL CLOCK/script.js
+++ DIGITAL CLOCK/script.js
@@ -13,24 +13,31 @@ setInterval(()=>{
     let s = new Date().getSeconds()
     let am =h>= 12? "PM":"AM"
 
+    
     //convert 24hr clock to 12hr clock
+    
     if (h>12){
         h=h-12
     }
 
     //add 0 before single digit number
+    
     h= (h<10)?"0"+h: h
     m= (m<10)?"0"+m: m
     s= (s<10)?"0"+s: s
 
-    hours.innerHTML=h //+ "<br><span>Hours</span></br>"
-    minutes.innerHTML=m //+ "<br><span>Minutes</span></br>"
-    seconds.innerHTML=s //+ "<br><span>Seconds</span></br>"
+    
+    hours.innerHTML=h
+    minutes.innerHTML=m 
+    seconds.innerHTML=s
     ampm.innerHTML=am
 
-    hh.style.strokeDashoffset= 440 - (440*h)/12 //12 hours
-    mm.style.strokeDashoffset= 440 - (440*m)/60 // 60 mins
-    ss.style.strokeDashoffset= 440 - (440*s)/60 // 60 seconds
+    hh.style.strokeDashoffset= 440 - (440*h)/12
+    //12 hours
+    mm.style.strokeDashoffset= 440 - (440*m)/60 
+    // 60 mins
+    ss.style.strokeDashoffset= 440 - (440*s)/60
+    // 60 seconds
 
     
 })
